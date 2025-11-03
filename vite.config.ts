734d7cc676a00f43b2ec2d6af8ddf20c7d184ec2
Scynes <skydust.developer@gmail.com>
2025-11-03 17:18:33 -0500

    deployment fix.

---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+
+// Replace "your-repo-name" with the actual name of your GitHub repository
+export default defineConfig({
+  base: '/blueprint-interactive-replica/',
+})
